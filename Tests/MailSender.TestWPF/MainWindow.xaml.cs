7dccb2072ecbe869b0d9fb6cd629d6efe2c5df28
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-12 14:27:51 +0300

    Механизм отправки почты работает

--- Tests/MailSender.TestWPF/MainWindow.xaml.cs
+++ Tests/MailSender.TestWPF/MainWindow.xaml.cs
@@ -10,28 +10,26 @@ namespace MailSender.TestWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            using var message = new MailMessage("zamanoff.renat@yandex.ru", "zamanov.renat@inbox.ru");
-            message.Subject = "Текстовое сообщение от " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff");
-            message.Body = "Тело текстового сообщения " + DateTime.Now.ToString("F");
+            using var message = new MailMessage("zamanoff.renat@yandex.ru", "zamanov.renat@mail.ru");
+            message.Subject = "Тестовое сообщение от " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff");
+            message.Body = "Тело тестового сообщения " + DateTime.Now.ToString("F");
 
-            using var client = new SmtpClient("smtp.yandex.ru", 465)
+            //message.Attachments.Add(new Attachment());
+
+            using var client = new SmtpClient("smtp.yandex.ru", 25)
             {
                 EnableSsl = true,
                 Credentials = new NetworkCredential
                 {
                     UserName = LoginEdit.Text,
-                    Password = PasswordEdit.SecurePassword.ToString(),
+                    SecurePassword = PasswordEdit.SecurePassword,
                 }
             };
-       
-            
+
             try
             {
                 client.Send(message);
@@ -39,7 +37,7 @@ namespace MailSender.TestWPF
             }
             catch (SmtpException smtp_exception)
             {
-                MessageBox.Show(smtp_exception.Message, "Ошибка при отправке почты", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show(smtp_exception.Message, "Ошибка при отправке почты", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
