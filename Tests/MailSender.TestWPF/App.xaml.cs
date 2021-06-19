2d8b136653982175234c1bf39c6fcbb1f6ed40b1
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-19 23:33:57 +0300

    Конфигурация файлa app, привязки титулов к вью модели

--- Tests/MailSender.TestWPF/App.xaml.cs
+++ Tests/MailSender.TestWPF/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+//using Microsoft.Extensions.Hosting;
+using MailSender.TestWPF.ViewModels;
+
 
 namespace MailSender.TestWPF
 {
@@ -13,5 +16,15 @@ namespace MailSender.TestWPF
     /// </summary>
     public partial class App : Application
     {
+        //private static IHost __Hosting;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ////Hosting.Start();
+            ////base.OnStartup(e);
+
+            //var services = new ServiceCollection();
+            //services.AddScoped<MainWindowViewModel>();
+        }
+
     }
 }
